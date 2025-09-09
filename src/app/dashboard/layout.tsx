64fff7051342ecdb40479869c348fb8b2ce1b0d5
FA BROKERS <admin@fabrokers.cl>
2025-09-09 11:24:20 +0000

    ok vamos a mejorar el fondo de la app, quiero que le coloques un color n

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -27,7 +27,7 @@ const AppContent = ({ children }: { children: ReactNode }) => {
     <div className="flex min-h-screen w-full">
       <div className="flex flex-1 flex-col">
         <Header />
-        <main className="flex-1 bg-background p-4 md:p-6 lg:p-8">
+        <main className="flex-1 bg-transparent p-4 md:p-6 lg:p-8">
           {children}
         </main>
       </div>
