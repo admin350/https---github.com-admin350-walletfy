3e4f31c36080e06fb6616ea71e681b0a7ccf1d02
FA BROKERS <admin@fabrokers.cl>
2025-09-16 02:09:57 +0000

    Try fixing this error: `Console Error: In HTML, <div> cannot be a child

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
       <DataProvider>
         <AppBody>
             {children}
+            <Toaster />
         </AppBody>
-        <Toaster />
       </DataProvider>
     </html>
   );
