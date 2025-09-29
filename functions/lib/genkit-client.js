28cf2a8dd1435874ea18023de08876e9dd2a0125
FA BROKERS <admin@fabrokers.cl>
2025-09-29 18:13:25 +0000

    me dio este error nuevamente corrige: studio-4002252363:~/studio/functio

--- functions/lib/genkit-client.js
+++ functions/lib/genkit-client.js
@@ -4,21 +4,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ai = void 0;
 const genkit_1 = require("genkit");
 const googleai_1 = require("@genkit-ai/googleai");
-const plugin_1 = require("@genkit-ai/firebase/plugin");
+const firebase_1 = require("@genkit-ai/firebase");
 exports.ai = (0, genkit_1.genkit)({
     plugins: [
         (0, googleai_1.googleAI)(),
-        (0, plugin_1.firebase)({
-            flowStateStore: {
-                collection: 'flow-states',
-            },
-            traceStore: {
-                collection: 'traces',
-            },
-            telemetry: {
-                sampler: {},
-            },
-        }),
+        (0, firebase_1.firebase)(),
     ],
+    flowStateStore: "firebase",
+    traceStore: "firebase",
+    logLevel: "debug",
+    enableTracingAndMetrics: true,
 });
 //# sourceMappingURL=genkit-client.js.map
