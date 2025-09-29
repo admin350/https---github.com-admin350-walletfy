48ff7fdc810fef2368f051962b073b0ce03347e7
FA BROKERS <admin@fabrokers.cl>
2025-09-29 19:54:20 +0000

    claro el gran detalle es que ahora esta consola la dejamos de usar recue

--- functions/lib/genkit-client.js
+++ functions/lib/genkit-client.js
@@ -4,18 +4,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ai = void 0;
 const genkit_1 = require("genkit");
 const googleai_1 = require("@genkit-ai/googleai");
-const firebase_1 = require("@genkit-ai/firebase");
 exports.ai = (0, genkit_1.genkit)({
     plugins: [
         (0, googleai_1.googleAI)(),
-        (0, firebase_1.firebase)({
-            flowStateStore: {
-                collection: 'flow-states',
-            },
-            traceStore: {
-                collection: 'traces',
-            },
-        }),
     ],
 });
 //# sourceMappingURL=genkit-client.js.map
